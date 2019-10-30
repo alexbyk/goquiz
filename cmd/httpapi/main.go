0b5ca3ab11b7b6a49fe4139ee713d4e8570bf5bc
alexbyk <alex@alexbyk.com>
2019-10-30 17:36:41 +0200

    feat: implement mock http api

---
+++ cmd/httpapi/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultListen = ":8080"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Recieved request: %s", b)
+	fmt.Fprint(w, "ok")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	var listen string
+	if listen = os.Getenv("API_LISTEN"); listen == "" {
+		listen = defaultListen
+	}
+	log.Fatal(http.ListenAndServe(defaultListen, nil))
+}
