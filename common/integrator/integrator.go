e3c3d09b85905e416833c5c0318f86e6bc6a81a1
alexbyk <alex@alexbyk.com>
2019-10-30 15:54:17 +0200

    feat: implement integrator logic

--- common/integrator/integrator.go
+++ common/integrator/integrator.go
@@ -2,9 +2,19 @@ package integrator
 
 import (
 	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"time"
 
 	"github.com/alexbyk/goquiz/common/model"
 )
+
+// DefaultDelayRetry is a default value for Integrator.DelayRetry
+const DefaultDelayRetry = time.Second * 10
+
+// DefaultDelayTick is a default value for Integrator.DelayTick
+const DefaultDelayTick = time.Second * 4
 
 // ErrEmpty is a flag that no records left
 var ErrEmpty = errors.New("No customers")
@@ -26,4 +36,113 @@ type Listener interface {
 // Publisher publish customers one by one to 3d party service
 type Publisher interface {
 	Publish(*model.Customer) error
+}
+
+// Logger is a simple logger interface
+type Logger interface {
+	Debug(...interface{})
+}
+
+// SimpleLogger is a dummy logger
+type SimpleLogger struct{}
+
+// Debug prints everything to the output
+func (logger *SimpleLogger) Debug(args ...interface{}) { log.Println(args...) }
+
+// Integrator sends data to the 3d party
+type Integrator struct {
+	nexter    Nexter
+	listener  Listener
+	publisher Publisher
+	Logger    Logger
+
+	// Delay between attempt to retry Publish
+	DelayRetry time.Duration
+
+	chExit chan struct{} // channel and a flag that we started
+
+	muRunning sync.Mutex
+	running   bool
+}
+
+// NewIntegrator returns an Integrator instance
+func NewIntegrator(n Nexter, l Listener, p Publisher) *Integrator {
+	return &Integrator{nexter: n, listener: l, publisher: p, DelayRetry: DefaultDelayRetry, Logger: &SimpleLogger{}}
+}
+
+// Start starts a daemon loop, sholdn't be called from multiple threads
+func (i *Integrator) Start() {
+
+	chListen := i.listener.Listen()
+	defer i.listener.Unlisten()
+
+	i.chExit = make(chan struct{})
+	defer close(i.chExit)
+
+	i.maybeRun() // run at the beginning
+	for {
+		select {
+		case <-i.chExit:
+			log.Println("exit")
+			return
+		case payload := <-chListen:
+			log.Println("Payload: ", payload)
+		}
+		i.maybeRun()
+	}
+}
+
+func (i *Integrator) maybeRun() {
+	i.muRunning.Lock() // run if not running already
+	if !i.running {
+		i.running = true
+		go i.run()
+	}
+	i.muRunning.Unlock()
+}
+
+// Stop stops the loop. Should be called only after Start
+func (i *Integrator) Stop() {
+	i.chExit <- struct{}{}
+	return
+}
+
+// blocking wait until time or stop
+func delay(d time.Duration, stop <-chan struct{}) {
+	timer := time.NewTimer(d)
+	defer timer.Stop()
+	select {
+	case <-stop:
+		return
+	case <-timer.C:
+	}
+}
+
+// Should be called from Start only
+func (i *Integrator) run() {
+	defer func() {
+		i.muRunning.Lock()
+		i.running = false
+		i.muRunning.Unlock()
+	}()
+
+	for {
+		// maybe stop? non-blocking
+		select {
+		case <-i.chExit:
+			return
+		default:
+		}
+
+		// call Next with a Publish closure. If ErrEmpty, return. Retry otherwise
+		err := i.nexter.Next(func(c *model.Customer) error { return i.publisher.Publish(c) })
+		switch {
+		case err == ErrEmpty:
+			return
+		case err != nil:
+			i.Logger.Debug(fmt.Sprintf("Error: %s, wating to repeat", err))
+			delay(i.DelayRetry, i.chExit)
+		}
+	}
+
 }
---
+++ common/integrator/integrator_test.go
@@ -0,0 +1,79 @@
+package integrator_test
+
+import (
+	"testing"
+
+	"github.com/alexbyk/ftest"
+	"github.com/alexbyk/goquiz/common/integrator"
+
+	"github.com/alexbyk/goquiz/common/model"
+)
+
+// Mocks
+type mNexter struct {
+	data []*model.Customer
+}
+
+func (m *mNexter) Next(fn func(*model.Customer) error) error {
+	if len(m.data) == 0 {
+		return integrator.ErrEmpty
+	}
+	var c *model.Customer
+	c = m.data[0]
+	err := fn(c)
+	if err == nil {
+		m.data = m.data[1:]
+	}
+	return err
+}
+
+type mListener struct {
+	c chan string
+}
+
+func (l *mListener) Listen() <-chan string {
+	l.c = make(chan string)
+	return l.c
+}
+
+func (l *mListener) Unlisten() error {
+	close(l.c)
+	return nil
+}
+
+type mPublisher struct {
+	data []error
+	got  []*model.Customer
+}
+
+func (p *mPublisher) Publish(c *model.Customer) error {
+	p.got = append(p.got, c)
+	if len(p.data) == 0 {
+		return nil
+	}
+	var e error
+	e, p.data = p.data[0], p.data[1:]
+	return e
+}
+
+type mLogger struct{}
+
+func (*mLogger) Debug(...interface{}) {}
+
+func TestUsage2(t *testing.T) {
+	ft := ftest.New(t)
+	l := new(mListener)
+	n := new(mNexter)
+	p := new(mPublisher)
+	intg := integrator.NewIntegrator(n, l, p)
+	intg.Logger = &mLogger{}
+	data := []*model.Customer{{ID: "1"}, {ID: "2"}}
+
+	n.data = data
+	go func() {
+		intg.Stop()
+	}()
+	intg.Start()
+
+	ft.Eq(p.got, data)
+}
